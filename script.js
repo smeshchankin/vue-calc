6b441e7d295298eacc02186be9093c016fcc89b9
Sergey Meshchankin <smeshchankin@gmail.com>
2020-10-27 22:21:28 +0200

    [CL-002] Add buttons handlers

--- script.js
+++ script.js
@@ -7,6 +7,17 @@
                 numbers: [7, 8, 9, 4, 5, 6, 1, 2, 3, 0],
                 operations: ['/', '*', '-', '+']
             };
+        },
+        methods: {
+            input: function(ch) {
+                this.result += ch;
+            },
+            reset: function() {
+                this.result = '';
+            },
+            calc: function() {
+                this.result = eval(this.result);
+            }
         }
     };
     Vue.createApp(App).mount('#app');
